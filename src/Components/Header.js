3376b1e0098335fae74c84a5c7d2c1a7ad8278fb
Adama SONKO <thioff1@gmail.com>
2020-06-04 17:05:45 +0200

    add banner

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import '../Styles/Header.css';
 import logo from "../Images/healthymood-logo.png";
+import banner from "../Images/healthymood-banner.jpg";
 
 function Header (){
     return(
-        <div className='header-container'>
-           <img src={logo} alt='logo'></img>
-        </div>
+        <header>
+            <img src={banner} alt='logo'></img>
+            <div className='header-container'>
+            <img src={logo} alt='logo'></img>
+            </div>
+        </header>
     )
 }
 
