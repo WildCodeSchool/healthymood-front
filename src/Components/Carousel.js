32cf3b085bad97fd3ccf42356d10adc84594deb9
Sora952 <sora952c16b10@gmail.com>
2020-06-04 15:32:14 +0200

    US5 Adding css

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -1,60 +1,63 @@
 import React, { useState } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../Styles/Carousel.css'
+import '../Styles/Carousel.css';
+
 function ControlledCarousel() {
   const [index, setIndex] = useState(0);
 
   const handleSelect = (selectedIndex, e) => {
     setIndex(selectedIndex);
   };
+  
+const ItemsCarousel = [
+  {  
+    className:'background-container d-block w-100',
+    src:'https://www.healthymood.fr/wp-content/uploads/legumes-ete-2.jpg',
+    alt:'First slide',
+    title: "Les fruits et légumes d'été sont arrivés!",
+    caption:"Découvrez plus de 150 recettes adaptées à la saison.",
+    button: "Lire l'article "
+  },
+  {  
+    className:"background-container d-block w-100",
+    src: "https://www.healthymood.fr/wp-content/uploads/smoothies-healthy-comfort-food.jpg",
+    alt: "Second slide",
+    title: "Découvrez nos astuces pour une cuisine plus saine",
+    caption: "Nous vous donnons toutes les clés pour cuisiner sainement en toute simplicité !",
+    button: "Lire nos conseils "
+  },
+  {  
+    className:'background-container d-block w-100',
+    src:'https://www.healthymood.fr/wp-content/uploads/nom7-2.jpg',
+    alt:'Third slide',
+    title: "L'annuaire de la cuisine saine et sans allergène !",
+    caption : "Healthymood, c'est plus de 500 recettes classées par catégorie. N'attendez plus et découvrez votre prochain repas !",
+    button: "Envoyer ma recette "
+  }
+];
 
-// a transformer en .map pour le rendre dynamique et qu'elle puisse le changer
-//crée tableau d'objet avec src image/lien etc ...
+
 
   return (
     
     <Carousel activeIndex={index} onSelect={handleSelect}>
-      <Carousel.Item>
-        <img
-          className="riri-class d-block w-100"
-          src="https://www.healthymood.fr/wp-content/uploads/legumes-ete-2.jpg"
-          alt="First slide"
-        />
-        <Carousel.Caption className='carousel-title'>
-          <h3>Les fruits et légumes d'été sont arrivés!</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          <button className='carousel-button'>Lire l'article &#62;</button>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://www.healthymood.fr/wp-content/uploads/smoothies-healthy-comfort-food.jpg"
-          alt="Second slide"
-        />
-
-        <Carousel.Caption className='carousel-title'>
-          <h3>Découvrez nos astuces pour une cuisine plus saine</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          <button className='carousel-button'>Lire nos conseils &#62;</button>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://www.healthymood.fr/wp-content/uploads/nom7-2.jpg"
-          alt="Third slide"
-        />
-
-        <Carousel.Caption className='carousel-title'>
-          <h3>Third slide label</h3>
-          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-          <button className='carousel-button'>Envoyer ma recette &#62;</button>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
-
+      {ItemsCarousel.map(e => {
+        return (
+          <Carousel.Item className='carousel-element' key={e.alt} >
+            <div className = {e.className} style={{backgroundImage:`url(${e.src})`}} />
+          <div className='carousel-content'>
+            <Carousel.Caption className='carousel-title'>
+              <h3>{e.title}</h3>
+              <p>{e.caption}</p>
+              <button className='carousel-button'>{e.button}</button>
+          </Carousel.Caption>
+          </div>          
+        </Carousel.Item>
+        )
+      }
+        )}
+        </Carousel>
   );
 }
 
