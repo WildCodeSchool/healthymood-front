26882132bea97f055286afcb14a73a56f6208e26
Adama SONKO <thioff1@gmail.com>
2020-06-04 18:19:18 +0200

    solved conflicts

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ControlledCarousel from '../Components/Carousel';
 
-
 class Home extends React.Component {
-    render() {
-        return (
-            <div className='home'>
-                <ControlledCarousel />
-            </div>
-        )
-    }
+  render () {
+    return (
+      <div className='home'>
+        <ControlledCarousel />
+      </div>
+    );
+  }
 }
 
 export default Home;
