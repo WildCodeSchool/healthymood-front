f0f1cfc3e61540db42d05750f08b1fd936b30f71
Riri Z <riri.zamor@gmail.com>
2020-06-09 12:04:50 +0200

    #7 creation handleChange

--- src/Components/Recherche.js
+++ src/Components/Recherche.js
@@ -1,10 +1,41 @@
-import React from 'react'
+import React from 'react';
 
 class Recherche extends React.Component {
-  render(){
+  constructor (props) {
+    super(props);
+    this.state = {
+      filter: ['chocolate', 'strawberry', 'vanilla'],
+      currentSearch: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange (event) {
+    this.setState({ currentSearch: event.target.value });
+  }
+
+  handleClick = () => {
+
+  }
+
+  render () {
     return (
-      <div>Hello</div>
-    )}
+      <div className='recherche-container'>
+        <div className='filter-list'>{this.state.filter.map(e => <p key={e}>{e}</p>)}</div>
+        <input
+          id='search'
+          name='search'
+          type='text'
+          value={this.state.currentSearch}
+          onChange={this.handleChange}
+        />
+        <button
+          onClick={this.handleClick}
+        >Rechercher
+        </button>
+      </div>
+    );
+  }
 }
 
 export default Recherche;
