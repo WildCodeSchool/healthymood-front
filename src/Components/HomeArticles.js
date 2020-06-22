256d0e7156b88cf6358f3a2d95b2a7a6ebc714d7
Geoffroy <geoffroy.crabieres@gmail.com>
2020-06-22 16:05:43 +0200

    created HomeArticles.js + CSS file

---
+++ src/Components/HomeArticles.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../Styles/HomeArticles.css';
+
+export default function HomeArticles() {
+    return (
+        <div className='homepage-articles-container'>
+
+        </div>
+    )
+}
