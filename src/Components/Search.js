ac267b0463aa93217775d205308a5dc3a31f62c6
Riri Z <riri.zamor@gmail.com>
2020-06-23 17:47:31 +0200

    all good

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,16 +1,16 @@
-import React from "react";
-import "../Styles/Search.css";
-import Loupe from "../Images/glass.png";
-import Cancel from "../Images/cross.png";
-import SmallRecipe from "./SmallRecipe";
-import API from "../Services/Api";
+import React from 'react';
+import '../Styles/Search.css';
+import Loupe from '../Images/glass.png';
+import Cancel from '../Images/cross.png';
+import SmallRecipe from './SmallRecipe';
+import API from '../Services/Api';
 class Search extends React.Component {
-  constructor(props) {
+  constructor (props) {
     super(props);
     this.state = {
       filter: [],
-      currentSearch: "",
-      recipes: [],
+      currentSearch: '',
+      recipes: []
     };
     this.handleGetRecipes = this.handleGetRecipes.bind(this);
   }
@@ -21,7 +21,7 @@ class Search extends React.Component {
       .then((res) => res.data)
       .then((data) => {
         this.setState({
-          recipes: [data],
+          recipes: [data]
         });
       });
   };
@@ -34,7 +34,7 @@ class Search extends React.Component {
     const currentFilter = [];
     const newFilter = currentFilter.concat(this.state.currentSearch);
     if (this.state.currentSearch) {
-      this.setState({ filter: newFilter, currentSearch: "" });
+      this.setState({ filter: newFilter, currentSearch: '' });
     }
     this.handleGetRecipes();
     this.props.history.push(`/rechercher/?search=${this.state.currentSearch}`);
@@ -43,12 +43,12 @@ class Search extends React.Component {
   handleDelete = (str) => {
     const newFilter = this.state.filter.filter((e) => str !== e);
     this.setState({ filter: newFilter, recipes: [] });
-    this.props.history.push("/rechercher");
+    this.props.history.push('/rechercher');
   };
 
   handleKeyDown = (event) => {
     // permet d'effectuer la recherche avec entrée
-    if (event.key === "Enter" && event.target.value) {
+    if (event.key === 'Enter' && event.target.value) {
       event.preventDefault();
       const currentSearch = event.target.value;
       this.setState({ currentSearch });
@@ -57,53 +57,53 @@ class Search extends React.Component {
     }
   };
 
-  render() {
+  render () {
     const recipes = this.state.recipes;
     return (
-      <div className="recherche-container">
-        <div className="Loupe">
+      <div className='recherche-container'>
+        <div className='Loupe'>
           <h5>Recherche simple</h5>
-          <div className="search-field">
-            <div className="filter-list">
+          <div className='search-field'>
+            <div className='filter-list'>
               {this.state.filter.map((e) => (
                 <p
                   key={e}
                   onClick={() => this.handleDelete(e)}
-                  className="filter-name"
+                  className='filter-name'
                 >
                   {e}
-                  <img src={Cancel} alt="cancel" />
+                  <img src={Cancel} alt='cancel' />
                 </p>
               ))}
             </div>
-            <div className="search-block">
-              <div className="my-search">
-                <label className="label">
+            <div className='search-block'>
+              <div className='my-search'>
+                <label className='label'>
                   <p>J'ai envie de : </p>
                 </label>
                 <input
-                  id="search"
-                  name="search"
-                  type="text"
-                  placeholder="Rechercher"
+                  id='search'
+                  name='search'
+                  type='text'
+                  placeholder='Rechercher'
                   value={this.state.currentSearch}
                   onChange={this.handleChange}
                   onKeyDown={this.handleKeyDown}
                 />
               </div>
-              <button className="btn-search" onClick={this.handleAddfilter}>
-                <img src={Loupe} alt="search" />
+              <button className='btn-search' onClick={this.handleAddfilter}>
+                <img src={Loupe} alt='search' />
                 Rechercher
               </button>
             </div>
-            <div className="result">
-              <div className="filter-recipes-container">
+            <div className='result'>
+              <div className='filter-recipes-container'>
                 {recipes.length === 0 ? (
                   <p>Entrez votre recherche.</p>
                 ) : (
                   recipes[0].data.map((recipe) => {
                     return (
-                      <div className="filtered-recipes" key={recipe.id}>
+                      <div className='filtered-recipes' key={recipe.id}>
                         <SmallRecipe r={recipe} />
                       </div>
                     );
--- src/Services/Api.js
+++ src/Services/Api.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios from 'axios';
 const baseURL = process.env.REACT_APP_API_BASE_URL;
 
 const API = axios.create({ baseURL });
