975a1984f1223884012538a7084b37f8fbc1ae1f
Riri Z <riri.zamor@gmail.com>
2020-06-23 17:23:09 +0200

    manifest json changé

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './Styles/index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import './Styles/Variables.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./Styles/index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import "./Styles/Variables.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
