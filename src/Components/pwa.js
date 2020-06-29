018a2701fbaef18a9aa268c8919c329c924a9559
Riri Z <riri.zamor@gmail.com>
2020-06-29 21:17:56 +0200

    standard fix

---
+++ src/Components/pwa.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import '../Styles/Pwa.css';
+
+class Pwa extends React.Component {
+  constructor () {
+    super();
+    this.state = {
+      showButton: false
+    };
+    this.handleOnInstallBtnClick = this.handleOnInstallBtnClick.bind(this);
+  }
+
+  componentDidMount () {
+    window.addEventListener('beforeinstallprompt', (e) => {
+      e.preventDefault();
+      this.deferredPrompt = e;
+      this.setState({
+        showButton: true
+      });
+    });
+  }
+
+  handleOnInstallBtnClick () {
+    this.setState({
+      showButton: false
+    });
+    this.deferredPrompt.prompt();
+    this.deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      this.deferredPrompt = null;
+    });
+  }
+
+  render () {
+    return (
+      <div>
+        <button className='add-install' onClick={this.handleOnInstallBtnClick}>
+          install
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Pwa;
