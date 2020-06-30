ac3079a354b7cfcb62cb472d7f12f473fbff8f74
Riri Z <riri.zamor@gmail.com>
2020-06-30 10:23:45 +0200

    delete this²

--- src/Components/pwa.js
+++ src/Components/pwa.js
@@ -1,41 +1,41 @@
-import React from "react";
-import "../Styles/Pwa.css";
+import React from 'react';
+import '../Styles/Pwa.css';
 
 let deferredPrompt;
 
 class Pwa extends React.Component {
-  constructor() {
+  constructor () {
     super();
     this.state = {};
     this.handleOnInstallBtnClick = this.handleOnInstallBtnClick.bind(this);
   }
 
-  componentDidMount() {
-    window.addEventListener("beforeinstallprompt", (e) => {
+  componentDidMount () {
+    window.addEventListener('beforeinstallprompt', (e) => {
       e.preventDefault();
       deferredPrompt = e;
     });
     /* window.alert(this.handleOnInstallBtnClick()); */
   }
 
-  handleOnInstallBtnClick() {
+  handleOnInstallBtnClick () {
     deferredPrompt.prompt();
     deferredPrompt.userChoice.then((choiceResult) => {
-      if (choiceResult.outcome === "accepted") {
-        console.log("User accepted the A2HS prompt");
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
       } else {
-        console.log("User dismissed the A2HS prompt");
+        console.log('User dismissed the A2HS prompt');
       }
       deferredPrompt = null;
     });
   }
 
-  render() {
+  render () {
     return (
       <div>
-         <button className="add-install" onClick={this.handleOnInstallBtnClick}>
+        <button className='add-install' onClick={this.handleOnInstallBtnClick}>
           install
-        </button> 
+        </button>
       </div>
     );
   }
