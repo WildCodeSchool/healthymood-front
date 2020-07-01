d878f5b00be741f5c23a7fec2fe389a9c707f4bf
Riri Z <riri.zamor@gmail.com>
2020-07-01 22:26:11 +0200

    semistandard fixx

--- src/Services/Api.js
+++ src/Services/Api.js
@@ -1,10 +1,10 @@
-import axios from "axios";
+import axios from 'axios';
 const baseURL = process.env.REACT_APP_API_BASE_URL;
 
 const API = axios.create({ baseURL });
 
 const placeTokenInRequestHeaders = (req) => {
-  const token = window.localStorage.getItem("authToken");
+  const token = window.localStorage.getItem('authToken');
   if (token) {
     req.headers.Authorization = `Bearer ${token}`;
   }
