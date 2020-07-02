b2b5956e23e5a2162a5f4a8a89dc1ebcac4a01d7
Sora952 <sora952c16b10@gmail.com>
2020-07-02 16:00:12 +0200

    create page compte and pwa alert adding

--- src/Pages/MonCompte.js
+++ src/Pages/MonCompte.js
@@ -1,10 +1,57 @@
-import React from 'react';
-// import API from '../Services/API';
+import React, { useState } from 'react';
+import '../Styles/Compte.css';
+import API from '../Services/API';
 
 const MonCompte = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const id = 1;
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    API.patch(`/users/${id}`, { email, password })
+      .then((res) => res.data)
+      .then((data) => {
+
+      })
+      .catch((err) => {
+        console.error(err);
+        window.alert('Mot de passe ou email incorrect.');
+      });
+  };
+
   return (
-    <div>
-      Modification de l'email dans une autre branche
+    <div className='compte-page'>
+      <h1 className='title-compte'>Changer ses infos</h1>
+      <form className='form-compte' onSubmit={handleSubmit}>
+        <div className='form-group'>
+          <label htmlFor='email' name='email'>
+            Email :
+          </label>
+          <input
+            id='email'
+            type='email'
+            value={email}
+            placeholder='NouvelEmail@gmail.com'
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='password' name='password'>
+            Mot de passe :
+          </label>
+          <input
+            id='password'
+            type='password'
+            value={password}
+            placeholder='Votre nouveau mot de passe'
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button className='btn-connexion' type='submit'>
+          Actualiser
+        </button>
+      </form>
     </div>
   );
 };
