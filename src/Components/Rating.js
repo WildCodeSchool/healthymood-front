452dae7c59fa38528ef3ee751f236fff752c561c
Riri Z <riri.zamor@gmail.com>
2020-07-08 12:06:33 +0200

    rating done

--- src/Components/Rating.js
+++ src/Components/Rating.js
@@ -1,58 +1,32 @@
-import React, { useState, useContext, useEffect } from 'react';
-import '../Styles/Rating.css';
-import { FaStar } from 'react-icons/fa';
-import AuthContext from '../Context/authContext';
-import API from '../Services/API';
+import React, { useState, useContext } from "react";
+import "../Styles/Rating.css";
+import { FaStar } from "react-icons/fa";
+import AuthContext from "../Context/authContext";
+import API from "../Services/API";
 
 const Rating = (props) => {
-  const [rating, setRating] = useState(null);
-  const [hover, setHover] = useState(null);
   const { connected } = useContext(AuthContext);
-
-  useEffect(() => {
-    API.get(`/ratings/${props.recipeInfo.id}`)
-      .then((res) => res.data)
-      .then((data) => {
-        const scoring = data.data.score;
-        setRating(scoring);
-      })
-      .catch((err) => {
-        console.error(err);
-        window.alert("Erreur lors de l'affichage de la notation");
-      });
-    // eslint-disable-next-line
-  }, []);
+  console.log(props);
+  const [rating, setRating] = useState(
+    props.recipeInfo.user_rating && props.recipeInfo.user_rating.score
+  );
+  const [hover, setHover] = useState(null);
 
   const handleSubmit = (event, ratingValue) => {
     event.preventDefault();
-    if (!ratingValue) {
-      setRating(ratingValue);
-      API.post('/ratings', {
-        score: ratingValue,
-        recipe_id: props.recipeInfo.id
+    API.post("/ratings", {
+      score: ratingValue,
+      recipe_id: props.recipeInfo.id,
+    })
+      .then((res) => res.data)
+      .then((data) => {
+        console.log({ data });
+        setRating(ratingValue);
       })
-        .then((res) => res.data)
-        .then((data) => {
-          console.log({ data });
-        })
-        .catch((err) => {
-          console.error(err);
-          window.alert('Erreur lors de la notation');
-        });
-    } else {
-      API.patch(`/ratings/${props.recipeInfo.id}`, {
-        score: ratingValue,
-        recipe_id: props.recipeInfo
-      })
-        .then((res) => res.data)
-        .then((data) => {
-          console.log({ data });
-        })
-        .catch((err) => {
-          console.error(err);
-          window.alert('Erreur lors de la notation');
-        });
-    }
+      .catch((err) => {
+        console.error(err);
+        window.alert("Erreur lors de la notation");
+      });
   };
 
   return (
@@ -64,14 +38,14 @@ const Rating = (props) => {
           return (
             <label key={ratingValue}>
               <input
-                type='radio'
-                className='rating'
+                type="radio"
+                className="rating"
                 value={ratingValue}
                 onClick={(event) => handleSubmit(event, ratingValue)}
               />
               <FaStar
-                className='star'
-                color={ratingValue <= (hover || rating) ? '#ffc107' : '#87CEEB'}
+                className="star"
+                color={ratingValue <= (hover || rating) ? "#ffc107" : "#87CEEB"}
                 sire={100}
                 onMouseEnter={() => setHover(ratingValue)}
                 onMouseOut={() => setHover(null)}
