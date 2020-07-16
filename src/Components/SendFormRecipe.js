537538b001072ab7e91c61ddb8cda0c3d915a042
Adama SONKO <thioff1@gmail.com>
2020-07-16 12:41:32 +0200

    first level email

--- src/Components/SendFormRecipe.js
+++ src/Components/SendFormRecipe.js
@@ -1,9 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import { useForm } from 'react-hook-form';
+import React, { useState } from 'react';
 import API from '../Services/Api';
 
 const SendFormRecipe = () => {
-  const url = '/recipe_categories';
+ /* const url = '/recipe_categories';
   const url2 = '/meal_types';
   const [regimeTypes, setRegimeTypes] = useState([]);
   const [mealTypes, setMealTypes] = useState([]);
@@ -18,32 +17,43 @@ const SendFormRecipe = () => {
       .then((data) => {
         setMealTypes(data.data);
       });
-  }, []);
-  console.log(mealTypes);
-  console.log(regimeTypes);
+  }, []);*/
 
-  const { register /*, handleSubmit*/ } = useForm();
-  /*const onSubmit = data => {
-    console.log(JSON.stringify(data));
-  };*/
+  const [inputs, setInputs] = useState({ username: '', email: '', title: '', description: '' });
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setInputs(prev => ({ ...prev, [name]: value }));
+  };
+  const handleSubmit = e => {
+    e.preventDefault();
+    const { username, email, title, description } = inputs;
+    API.post('/post_recipe', {
+      username,
+      email,
+      title,
+      text: description
+    });
+    console.log(inputs);
+  };
 
   return (
     <div className='form-recipe'>
       <h2>Proposer ma recette</h2>
-      <form id='post-recipe-form' action='http://localhost:4000/post_recipe' method='POST'>
+      <form id='post-recipe-form' onSubmit={handleSubmit} method='POST'>
         <div className='author-id'>
           <div className='author-username'>
             <label>Pseudo : </label>
-            <input type='text' name='username' ref={register} placeholder='Entrez votre pseudo' />
+            <input type='text' name='username' value={inputs.username} onChange={handleChange} placeholder='Entrez votre pseudo' />
           </div>
           <div className='author-mail'>
             <label>Email : </label>
-            <input type='email' aria-describedby='emailHelp' name='email' ref={register} placeholder='Entrez votre adresse email' />
+            <input type='email' name='email' value={inputs.email} onChange={handleChange} placeholder='Entrez votre adresse email' />
           </div>
         </div>
         <div className='form-title'>
           <label>Titre : </label>
-          <input type='text' name='title' ref={register} placeholder='Entrez un Titre' />
+          <input type='text' name='title' value={inputs.title} onChange={handleChange} placeholder='Entrez un Titre' />
         </div>
         <p>Choisissez un ou plusieurs critères :</p>
         {/* <div className='regime-type-select'>
@@ -56,7 +66,7 @@ const SendFormRecipe = () => {
   </div> */}
         <div className='recipe-post-description'>
           <label>Description de ma recette : </label>
-          <textarea name='description' ref={register} placeholder='Détaillez votre recette' />
+          <textarea name='description' value={inputs.description} onChange={handleChange} placeholder='Détaillez votre recette' />
         </div>
         {/* <div className='download'>
           <label>Télécharger une image : </label>
