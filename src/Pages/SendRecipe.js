c585c51db2aad6e1c1b053dcb275be7d964ba3b6
Geoffroy <geoffroy.crabieres@gmail.com>
2020-07-17 11:04:43 +0200

    updated send recipe form css

--- src/Pages/SendRecipe.js
+++ src/Pages/SendRecipe.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import SendFormRecipe from '../Components/SendFormRecipe';
+import '../Styles/SendRecipe.css';
 
 class SendRecipe extends React.Component {
-  render () {
+  render() {
     return (
       <div className='send-form-recipe'>
         <SendFormRecipe />
