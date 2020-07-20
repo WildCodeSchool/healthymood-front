2b2760d73b18102f646e319273c4bb380215ea85
Geoffroy <geoffroy.crabieres@gmail.com>
2020-07-20 17:10:58 +0200

    using slug instead of id on single article page

--- src/Pages/Article.js
+++ src/Pages/Article.js
@@ -4,7 +4,7 @@ import '../Styles/Article.css';
 import API from '../Services/API';
 
 export default class Article extends Component {
-  constructor(props) {
+  constructor (props) {
     super(props);
     this.state = {
       data: [],
@@ -12,7 +12,7 @@ export default class Article extends Component {
     };
   }
 
-  componentDidMount() {
+  componentDidMount () {
     const searchOrId = this.props.match.params.slug;
     API.get(`/articles/${searchOrId}`)
       .then((res) => res.data)
@@ -22,18 +22,18 @@ export default class Article extends Component {
       .then((data) => this.setState({ data, articleIsLoading: false }));
   }
 
-  render() {
+  render () {
     return (
       <div className='page-article'>
         {this.state.articleIsLoading ? (<p>Chargement</p>
         ) : (
-            <>
-              <h1>{this.state.data.title}</h1>
-              <div className='article-content-container'>
-                <ArticleContent a={this.state.data} />
-              </div>
-            </>
-          )}
+          <>
+            <h1>{this.state.data.title}</h1>
+            <div className='article-content-container'>
+              <ArticleContent a={this.state.data} />
+            </div>
+          </>
+        )}
       </div>
     );
   }
